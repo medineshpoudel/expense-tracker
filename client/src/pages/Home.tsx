11daf1f32af02623011e21d8222c70e7f4ae9e78
dinesh poudel <dineshpoudel58@gmail.com>
2023-12-27 09:03:35 +0545

    ET-4-DP: setup redux toolkit in the app.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState } from '../store/store';
+import { decreament, increament } from '../store/counter/counterSlice';
 
 const Home = () => {
-  console.log('hi');
-  return <h1> hi </h1>;
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button type="button" onClick={() => dispatch(increament())}>
+        +{' '}
+      </button>
+      <button type="button" onClick={() => dispatch(decreament())}>
+        -
+      </button>
+    </div>
+  );
 };
 
 export default Home;
